65e2df0fe617c5d42a3d821cd76b1eda5373795b
joaquin <joaquinroman2006@gmail.com>
2024-12-04 18:07:56 -0300

    Cambios necesarios para que se pueda agregar el producto con imageen

--- src/Componentes/ProductosAdmin.jsx
+++ src/Componentes/ProductosAdmin.jsx
@@ -8,13 +8,20 @@ class ProductosAdmin extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      nuevoProducto: {
+        nombre: '',
+        categoria: '',
+        precio: '',
+        stock: '',
+        descripcion: '',
+        marca: '',
+        imagen: null,  // Inicializa el valor de imagen
+      },
       productos: [],
-      nuevoProducto: { nombre: '', categoria: '', precio: '', stock: '', descripcion: '', marca: '' },
       editando: false,
-      productoEditadoId: null,
     };
   }
-
+  
   componentDidMount() {
     // Suponiendo que el nombre del usuario viene de un token o API
     const usuario = localStorage.getItem('usuario'); // o desde una API que traiga el usuario logueado
@@ -42,40 +49,42 @@ class ProductosAdmin extends Component {
     });
   }
 
-  agregarProducto = () => {
-    const { nuevoProducto, imagenes } = this.state; // Asumiendo que 'imagenes' es un arreglo de archivos seleccionados
-    const formData = new FormData();
+  handleFileChange = (e) => {
+    this.setState({ imagenes: e.target.files });
+  }
+  
 
-    // Agregar los datos del producto al FormData
+  agregarProducto = () => {
+    const { nuevoProducto, imagenes } = this.state;
+  
+    const formData = new FormData();
     formData.append('nombre', nuevoProducto.nombre);
     formData.append('stock', nuevoProducto.stock);
     formData.append('precio', nuevoProducto.precio);
     formData.append('descrip', nuevoProducto.descripcion);
     formData.append('marca', nuevoProducto.marca);
-
-    // Si hay imágenes, agregarlas al FormData
+  
+    // Agregar las imágenes al FormData
     if (imagenes && imagenes.length > 0) {
-        imagenes.forEach(imagen => {
-            formData.append('imagen', imagen); // Asegúrate de que 'imagen' sea el nombre correcto que usa el backend
-        });
+      Array.from(imagenes).forEach(imagen => {
+        formData.append('imagen', imagen);
+      });
     }
-
-    // Realizamos la solicitud POST con axios usando promesas
+  
     axios.post('http://localhost:4000/api/admin/productos', formData, {
-        headers: {
-            'Content-Type': 'multipart/form-data', // Indicamos que estamos enviando un formulario con archivos
-        },
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
     })
     .then(response => {
-        console.log('Producto agregado con éxito:', response);
-        // Aquí puedes actualizar el estado o hacer algo con la respuesta, como mostrar un mensaje de éxito o redirigir
+      console.log('Producto agregado con éxito:', response);
+      this.setState({ productos: [...this.state.productos, response.data], nuevoProducto: {}, imagenes: [] });
     })
     .catch(error => {
-        console.error('Error al agregar producto:', error);
-        // Aquí puedes manejar el error, mostrando un mensaje al usuario
+      console.error('Error al agregar producto:', error);
     });
-};
-
+  }
+  
 
   iniciarEdicion = (producto) => {
     this.setState({
@@ -101,103 +110,114 @@ class ProductosAdmin extends Component {
 };
 
 
-  render() {
-    return (
-      <div className="d-flex min-vh-100 bg-light">
-        {/* Sidebar */}
-        <aside className="mr-4" style={{ width: '250px' }}>
-          <nav className="d-flex flex-column">
-            <Link to="/admin" className="text-left mb-2 d-flex align-items-center btn btn-light">
-              <LayoutDashboard className="mr-2" />
-              Panel de control
-            </Link>
-            <Button variant="light" className="text-left mb-2 d-flex align-items-center">
-              <Package className="mr-2" />
-              Productos
+render() {
+  return (
+    <div className="d-flex min-vh-100 bg-light">
+      {/* Sidebar */}
+      <aside className="mr-4" style={{ width: '250px' }}>
+        <nav className="d-flex flex-column">
+          <Link to="/admin" className="text-left mb-2 d-flex align-items-center btn btn-light">
+            <LayoutDashboard className="mr-2" />
+            Panel de control
+          </Link>
+          <Button variant="light" className="text-left mb-2 d-flex align-items-center">
+            <Package className="mr-2" />
+            Productos
+          </Button>
+          <Link to="/admin/usuarios" className="text-left mb-2 d-flex align-items-center btn btn-light">
+            <Users className="mr-2" />
+            Usuarios
+          </Link>
+          <Link to="/admin/ventas" className="text-left mb-2 d-flex align-items-center btn btn-light">
+            <ShoppingCart className="mr-2" />
+            Ventas
+          </Link>
+        </nav>
+      </aside>
+
+      {/* Main Content */}
+      <main className="flex-grow-1 container">
+        {/* Formulario para agregar o editar producto */}
+        <div className="py-4">
+          <h5>{this.state.editando ? "Editar Producto" : "Agregar Nuevo Producto"}</h5>
+          <Form className="d-flex flex-wrap gap-3">
+            <FormControl
+              placeholder="Nombre del producto"
+              name="nombre"
+              value={this.state.nuevoProducto.nombre || ''}  // Asegúrate de que 'nombre' no sea undefined
+              onChange={this.handleInputChange}
+              style={{ maxWidth: '200px' }}
+            />
+            <FormControl
+              placeholder="Categoría"
+              name="categoria"
+              value={this.state.nuevoProducto.categoria || ''}
+              onChange={this.handleInputChange}
+              style={{ maxWidth: '150px' }}
+            />
+            <FormControl
+              placeholder="Precio"
+              name="precio"
+              value={this.state.nuevoProducto.precio || ''}
+              onChange={this.handleInputChange}
+              style={{ maxWidth: '100px' }}
+            />
+            <FormControl
+              placeholder="Stock"
+              name="stock"
+              value={this.state.nuevoProducto.stock || ''}
+              onChange={this.handleInputChange}
+              style={{ maxWidth: '100px' }}
+            />
+            <FormControl
+              placeholder="Descripción"
+              name="descripcion"
+              value={this.state.nuevoProducto.descripcion || ''}
+              onChange={this.handleInputChange}
+              style={{ maxWidth: '250px' }}
+            />
+            <FormControl
+              placeholder="Marca"
+              name="marca"
+              value={this.state.nuevoProducto.marca || ''}
+              onChange={this.handleInputChange}
+              style={{ maxWidth: '150px' }}
+            />
+
+            {/* Campo para seleccionar imagen */}
+            <Form.Group>
+              <Form.Label>Cargar Imagen</Form.Label>
+              <Form.Control
+                type="file"
+                onChange={this.handleFileChange}
+                multiple
+              />
+            </Form.Group>
+
+            <Button variant="dark" onClick={this.agregarProducto}>
+              {this.state.editando ? "Guardar Cambios" : "Agregar"}
             </Button>
-            <Link to="/admin/usuarios" className="text-left mb-2 d-flex align-items-center btn btn-light">
-              <Users className="mr-2" />
-              Usuarios
-            </Link>
-            <Link to="/admin/ventas" className="text-left mb-2 d-flex align-items-center btn btn-light">
-              <ShoppingCart className="mr-2" />
-              Ventas
-            </Link>
-          </nav>
-        </aside>
+          </Form>
+        </div>
 
-        {/* Main Content */}
-        <main className="flex-grow-1 container">
-          {/* Formulario para agregar o editar producto */}
-          <div className="py-4">
-            <h5>{this.state.editando ? "Editar Producto" : "Agregar Nuevo Producto"}</h5>
-            <Form className="d-flex flex-wrap gap-3">
-              <FormControl
-                placeholder="Nombre del producto"
-                name="nombre"
-                value={this.state.nuevoProducto.nombre}
-                onChange={this.handleInputChange}
-                style={{ maxWidth: '200px' }}
-              />
-              <FormControl
-                placeholder="Categoría"
-                name="categoria"
-                value={this.state.nuevoProducto.categoria}
-                onChange={this.handleInputChange}
-                style={{ maxWidth: '150px' }}
-              />
-              <FormControl
-                placeholder="Precio"
-                name="precio"
-                value={this.state.nuevoProducto.precio}
-                onChange={this.handleInputChange}
-                style={{ maxWidth: '100px' }}
-              />
-              <FormControl
-                placeholder="Stock"
-                name="stock"
-                value={this.state.nuevoProducto.stock}
-                onChange={this.handleInputChange}
-                style={{ maxWidth: '100px' }}
-              />
-              <FormControl
-                placeholder="Descripción"
-                name="descripcion"
-                value={this.state.nuevoProducto.descripcion}
-                onChange={this.handleInputChange}
-                style={{ maxWidth: '250px' }}
-              />
-              <FormControl
-                placeholder="Marca"
-                name="marca"
-                value={this.state.nuevoProducto.marca}
-                onChange={this.handleInputChange}
-                style={{ maxWidth: '150px' }}
-              />
-              <Button variant="dark" onClick={this.agregarProducto}>
-                {this.state.editando ? "Guardar Cambios" : "Agregar"}
-              </Button>
-            </Form>
-          </div>
-
-          {/* Products Table */}
-          <div className="py-4">
-            <Table striped bordered hover responsive>
-              <thead>
-                <tr>
-                  <th>Nombre</th>
-                  <th>Categoría</th>
-                  <th>Precio</th>
-                  <th>Stock</th>
-                  <th>Descripción</th>
-                  <th>Marca</th>
-                  <th>Acciones</th>
-                </tr>
-              </thead>
-              <tbody>
+        {/* Products Table */}
+        <div className="py-4">
+          <Table striped bordered hover responsive>
+            <thead>
+              <tr>
+                <th>Nombre</th>
+                <th>Categoría</th>
+                <th>Precio</th>
+                <th>Stock</th>
+                <th>Descripción</th>
+                <th>Marca</th>
+                <th>Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
               {Array.isArray(this.state.productos) && this.state.productos.length > 0 ? (
-                this.state.productos.map((producto) => (
-                  <tr key={producto.id}>
+                this.state.productos.map((producto, index) => (
+                  <tr key={producto.id || index}> {/* Usar index como fallback */}
                     <td>{producto.nombre}</td>
                     <td>{producto.categoria}</td>
                     <td>{producto.precio}</td>
@@ -205,14 +225,14 @@ class ProductosAdmin extends Component {
                     <td>{producto.descripcion}</td>
                     <td>{producto.marca}</td>
                     <td>
-                    <div className="d-flex gap-2">
-                      <Button variant="light" className="p-1" onClick={() => this.iniciarEdicion(producto)}>
-                        <Edit2 size={16} />
-                      </Button>
-                      <Button variant="light" className="p-1" onClick={() => this.eliminarProducto(producto.id)}>
-                        <Trash2 size={16} />
-                      </Button>
-                    </div>
+                      <div className="d-flex gap-2">
+                        <Button variant="light" className="p-1" onClick={() => this.iniciarEdicion(producto)}>
+                          <Edit2 size={16} />
+                        </Button>
+                        <Button variant="light" className="p-1" onClick={() => this.eliminarProducto(producto.id)}>
+                          <Trash2 size={16} />
+                        </Button>
+                      </div>
                     </td>
                   </tr>
                 ))
@@ -222,12 +242,12 @@ class ProductosAdmin extends Component {
                 </tr>
               )}
             </tbody>
-            </Table>
-          </div>
-        </main>
-      </div>
-    );
-  }
+          </Table>
+        </div>
+      </main>
+    </div>
+  );
+}
 }
 
 export default ProductosAdmin;
