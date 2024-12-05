23515213f227164c1cdb0a9b99ac26a65c0de976
joaquin <joaquinroman2006@gmail.com>
2024-12-05 02:57:42 -0300

    testeando

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
+import './App.css';  // o el archivo correspondiente con los estilos globales
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Componentes/Header';
 import NavBar from './Componentes/NavBar';
@@ -14,14 +14,14 @@ import Admin from './Componentes/Admin';
 import ProductosAdmin from './Componentes/ProductosAdmin';
 import UsuariosAdmin from './Componentes/UsuariosAdmin';
 import VentasAdmin from "./Componentes/VentasAdmin";
-import PrivateRoute from './Componentes/PrivateRoute';
-import FormularioCompra from './Componentes/FormularioCompra';
+import PrivateRoute from './Componentes/PrivateRoute';  // Importa el componente PrivateRoute
+import FormularioCompra from './Componentes/FormularioCompra'; // Importa el componente
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchTerm: '',
+      searchTerm: ''
     };
   }
 
@@ -38,37 +38,48 @@ class App extends Component {
           <NavBar />
           
           <main className="flex-grow-1 container my-4">
-            <Routes>
-              <Route path="/" element={<Inicio searchTerm={this.state.searchTerm} />} />
-              <Route path="/productos" element={<Productos />} />
-              <Route path="/registro" element={<MainForm />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/carrito" element={<CarritoProductos />} />
-              <Route path="/producto/:id" element={<ProductoDetallado />} />
-              
-              {/* Rutas protegidas de administración */}
-              <Route path="/admin" element={
+          <Routes>
+            <Route path="/" element={<Inicio searchTerm={this.state.searchTerm} />} />
+            <Route path="/productos" element={<Productos />} />
+            <Route path="/registro" element={<MainForm />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/carrito" element={<CarritoProductos />} />
+            <Route path="/producto/:id" element={<ProductoDetallado />} />
+
+            {/* Rutas protegidas */}
+            <Route 
+              path="/admin" 
+              element={
                 <PrivateRoute>
                   <Admin />
                 </PrivateRoute>
-              } />
-              <Route path="/admin/productos" element={
+              } 
+            />
+            <Route 
+              path="/admin/productos" 
+              element={
                 <PrivateRoute>
                   <ProductosAdmin />
                 </PrivateRoute>
-              } />
-              <Route path="/admin/usuarios" element={
+              } 
+            />
+            <Route 
+              path="/admin/usuarios" 
+              element={
                 <PrivateRoute>
                   <UsuariosAdmin />
                 </PrivateRoute>
-              } />
-              <Route path="/admin/ventas" element={
+              } 
+            />
+            <Route 
+              path="/admin/ventas" 
+              element={
                 <PrivateRoute>
                   <VentasAdmin />
                 </PrivateRoute>
-              } />
-              <Route path="/formulario-compra" element={<FormularioCompra />} />
-            </Routes>
+              } 
+            />
+          </Routes>
           </main>
         </div>
       </Router>
